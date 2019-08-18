f92b4ec5095fa3560612ba5984c65127ed0b6d76
kjdekajlo <kjdekajlo@gmail.com>
2019-08-18 18:33:24 +0200

    changed few things tried to implement title text changing with js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
         window.location.reload(1);
     });
 
-    var pageWidth = $("body").width();
+    const pageWidth = $("body").width();
     if (pageWidth < 800) {
         $("#hamburger").click(function () {
             $("#sidenav").animate({
@@ -20,11 +20,14 @@ $(document).ready(function () {
 
     }
 
+    var title = ["student", "developer", "gamer", "translator"];
 
 
-
-
-
+    for (var i = 0; i < title.length; i++) {
+        window.setInterval(function() {
+            $("#title").text(title[i]);
+        }, 5000);
+    }
 
 
 });
